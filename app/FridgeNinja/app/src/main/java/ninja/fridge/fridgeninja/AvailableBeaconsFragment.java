23d5052a61c812e5e4b9340a25eb81e7da271814
Benedikt Brandner <ga49xel@mytum.de>
2015-10-17 19:29:00 +0200

    open & closed states for accelerometer

--- app/FridgeNinja/app/src/main/java/ninja/fridge/fridgeninja/AvailableBeaconsFragment.java
+++ app/FridgeNinja/app/src/main/java/ninja/fridge/fridgeninja/AvailableBeaconsFragment.java
@@ -26,8 +26,6 @@ import ninja.fridge.fridgeninja.dummy.DummyContent;
  * Large screen devices (such as tablets) are supported by replacing the ListView
  * with a GridView.
  * <p/>
- * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
- * interface.
  */
 public class AvailableBeaconsFragment extends Fragment implements AbsListView.OnItemClickListener,
         MainActivity.BeaconsUpdateCallback {
@@ -165,21 +163,6 @@ public class AvailableBeaconsFragment extends Fragment implements AbsListView.On
             }
         });
 
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(String id);
     }
 
 }
--- app/FridgeNinja/app/src/main/java/ninja/fridge/fridgeninja/FridgeInterfaceFragment.java
+++ app/FridgeNinja/app/src/main/java/ninja/fridge/fridgeninja/FridgeInterfaceFragment.java
@@ -1,23 +1,27 @@
 package ninja.fridge.fridgeninja;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FridgeInterfaceFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link FridgeInterfaceFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FridgeInterfaceFragment extends Fragment {
+public class FridgeInterfaceFragment extends Fragment implements SensorEventListener {
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
@@ -27,7 +31,7 @@ public class FridgeInterfaceFragment extends Fragment {
 //    private String mParam1;
 //    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private Activity mActivity;
 
     /**
      * Use this factory method to create a new instance of
@@ -65,43 +69,137 @@ public class FridgeInterfaceFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_fridge_interface, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        mActivity = activity;
+
+        sensorMan = (SensorManager)activity.getSystemService(Context.SENSOR_SERVICE);
+        gyro = sensorMan.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        mActivity = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    private SensorManager sensorMan;
+    private Sensor gyro;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorMan.registerListener(this, gyro,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorMan.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
+            float[] force = event.values.clone();
+            // Shake detection
+            float x = force[0];
+            float y = force[1];
+            float z = force[2];
+//            Log.i("Sensors", String.format("x: %.2f y: %.2f z: %.2f", x, y, z));
+
+            onGyroData(x, y, z);
+        }
+
+    }
+
+    private LinkedList<Float> cache = new LinkedList<>();
+    private int cacheSize = 8;
+    private final float magicOpenConstant = 0.4f;
+
+    private boolean closed = true;
+    private boolean opening = false;
+    private boolean closing = false;
+    private int openDirection = 0; // 0: unknown -1: negative opens 1: positive opens
+
+    private void onGyroData(float x, float y, float z) {
+        float abs = Math.abs(y);
+        while(cache.size() >= cacheSize) {
+            cache.removeFirst();
+        }
+        cache.addLast(abs);
+
+        float avg = 0;
+        if(!cache.isEmpty()) {
+            for(Float v : cache) {
+                avg += v;
+            }
+            avg /= cache.size();
+        }
+
+        if(avg > magicOpenConstant) {
+            // first run: determine if positive opens or closes
+            if(openDirection == 0 && y > magicOpenConstant) {
+                if(y < 0) {
+                    openDirection = -1;
+                } else {
+                    openDirection = 1;
+                }
+            }
+
+            // opening or closing
+            if(closed) {
+                opening = true;
+            } else {
+                closing = true;
+            }
+        } else {
+            // open or closed if we did something
+            if(opening) {
+                closed = false;
+                opening = false;
+            }
+            if(closing) {
+                closed = true;
+                closing = false;
+            }
+        }
+
+        String text = "Last " + cache.size() + ": " + String.format("%.2f", avg) + "\n";
+        for(float v : cache) {
+            text += String.format("%.2f", v) + "\n";
+        }
+
+        String state;
+        if(opening || closing) {
+            state = opening ? "OPENING" : "CLOSING";
+        } else {
+            state = closed ? "CLOSED" : "OPEN";
+        }
+
+        final String stateCopy = state;
+        final String copy = text;
+        if(mActivity != null) {
+            mActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    View root = getView();
+                    if(root != null) {
+                        TextView v = (TextView) root.findViewById(android.R.id.text1);
+                        v.setText(copy);
+                        TextView vs = (TextView) root.findViewById(R.id.openimg);
+                        vs.setText(stateCopy);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // required method
     }
 
 }
