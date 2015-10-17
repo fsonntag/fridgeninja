e410a1c2a3e8da728317e8182213aa04ee63b8a7
Felix Sonntag <felix.sonntag@outlook.com>
2015-10-17 23:02:19 +0200

    Adjusted event string Adjusted mock data

--- backend/mock_data.py
+++ backend/mock_data.py
@@ -13,15 +13,15 @@ requests.post("http://" + host + ":" + port + "/users/", data = json.dumps(user1
 requests.post("http://" + host + ":" + port + "/users/", data = json.dumps(user2))
 requests.post("http://" + host + ":" + port + "/users/", data = json.dumps(user3))
 
-event1 = {"username": "Bene", "transactions": {"Bier": 20, "Wasser": 10, "Limo": 2, "Kekse": 3}}
-event2 = {"username": "Felix", "transactions": {"Bier": -1}}
-event3 = {"username": "Bene", "transactions": {"Bier": -3}}
-event4 = {"username": "Philipp", "transactions": {"Bier": -5, "Wasser": -1, "Vodka": 1}}
-event5 = {"username": "Philipp", "transactions": {"Limo": -1}}
-event6 = {"username": "Bene", "transactions": {"Wasser": -1, "Mate": 1}}
-event7 = {"username": "Felix", "transactions": {"Bier": -5, "Wasser": -2, "Apfel": 2}}
-event8 = {"username": "Bene", "transactions": {"Bier": -6}}
-event9 = {"username": "Felix", "transactions": {"Bier": 20, "Wasser": 10, "Limo": 2, "Doener": -1}}
+event1 = {"username": "Bene", "transactions": {"beer": 20, "water": 10, "lemonade": 2, "Jagermeister": 3}}
+event2 = {"username": "Felix", "transactions": {"beer": -1}}
+event3 = {"username": "Bene", "transactions": {"beer": -3}}
+event4 = {"username": "Philipp", "transactions": {"beer": -5, "water": -1, "vodka": 1}}
+event5 = {"username": "Philipp", "transactions": {"lemonade": -1}}
+event6 = {"username": "Bene", "transactions": {"water": -1, "Spezi": 1}}
+event7 = {"username": "Felix", "transactions": {"beer": -5, "water": -2, "coke": 2}}
+event8 = {"username": "Bene", "transactions": {"beer": -6}}
+event9 = {"username": "Felix", "transactions": {"beer": 20, "water": 10, "lemonade": 2, "juice": -1}}
 
 requests.post("http://" + host + ":" + port + "/events/", data = json.dumps(event1))
 requests.post("http://" + host + ":" + port + "/events/", data = json.dumps(event2))
--- frontend/js/events.js
+++ frontend/js/events.js
@@ -1,16 +1,69 @@
 function append_event_content(timestamp, username, transactions) {
     var grid = document.querySelector('#event_columns');
     var item = document.createElement('div');
-    var h = '<div class="col-md-3">';
-    h += '<div class="">';
-    h += username;
+    var date = new Date(timestamp);
+    var transaction_string = build_transaction_string(username, transactions);
+    var h = '<div class="">';
+    h += moment(date).format('MMM D YYYY, h:mm:s a');
     h += '</div>';
     h += '<div class="">';
-    h += timestamp;
-    h += '</div>';
+    h += transaction_string;
     h += '</div>';
     salvattore['append_elements'](grid, [item]);
     item.outerHTML = h;
+}
+
+function build_transaction_string(username, transactions) {
+  var s = username + " ";
+  var plus_transactions = {};
+  var minus_transactions = {};
+  for (var key in transactions) {
+    if (transactions[key] >= 0) {
+      plus_transactions[key] = transactions[key];
+    } else {
+      minus_transactions[key] = transactions[key];
+    }
+  }
+
+  var plus_s = "";
+  var minus_s = "";
+  var plus_keys = Object.keys(plus_transactions);
+  var minus_keys = Object.keys(minus_transactions);
+
+  for (i = 0; i < plus_keys.length; i++) {
+    if (i == plus_keys.length - 1 && plus_keys.length != 1) {
+      plus_s = plus_s.substring(0, plus_s.length - 2) + " and ";
+    }
+
+    plus_s += plus_transactions[plus_keys[i]] + " " + plus_keys[i];
+    if (plus_transactions[plus_keys[i]] == 1) {
+      plus_s += ", ";
+    } else {
+      plus_s += "s, ";
+    }
+  }
+
+  for (i = 0; i < minus_keys.length; i++) {
+    if (i == minus_keys.length - 1 && minus_keys.length != 1) {
+      minus_s = minus_s.substring(0, plus_s.length - 2) + " and ";
+    }
+
+    minus_s += Math.abs(minus_transactions[minus_keys[i]]) + " " + minus_keys[i];
+    if (minus_transactions[minus_keys[i]] == -1) {
+      minus_s += ", ";
+    } else {
+      minus_s += "s, ";
+    }
+  }
+
+  if (plus_keys.length > 0 && minus_keys.length > 0) {
+    s += "took " + plus_s.substring(0, plus_s.length - 2) + " and put in " + minus_s.substring(0, minus_s.length - 2) + ".";
+  } else if (plus_keys.length > 0 && minus_keys.length == 0) {
+    s += "took " + plus_s.substring(0, plus_s.length - 2) + ".";
+  } else {
+    s += "put in " + minus_s.substring(0, minus_s.length - 2) + ".";
+  }
+  return s;
 }
 
 $.getJSON("/events/", function (data) {
