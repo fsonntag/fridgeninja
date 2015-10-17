6b54ab50edef0a0116a1b21402753ea29beab297
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-10-18 00:44:00 +0200

    oops

--- backend/endpoints/inventory.py
+++ backend/endpoints/inventory.py
@@ -11,4 +11,6 @@ def get_inventory():
 
     return jsonify(inventory=[{"name": item, "quantity": count} for item, count in fridge.content.items()])
 
-g
+@fridge_blueprint.route("/choices", methods=["GET"]
+def get_item_suggestions():
+    return "null" # todo
