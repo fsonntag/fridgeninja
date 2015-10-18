5a317ea27d4647a4b0238e30c07261b3c266a877
Benedikt Brandner <ga49xel@mytum.de>
2015-10-18 05:08:47 +0200

    secret shit works

--- frontend/js/secret.js
+++ frontend/js/secret.js
@@ -11,72 +11,121 @@ var getUrlParameter = function getUrlParameter(sParam) {
             return sParameterName[1] === undefined ? true : sParameterName[1];
         }
     }
+
+    return "Bene";
 };
 
-username = getUrlParameter("username");
+var currentuser = getUrlParameter("username");
 
-function append_fridge_content(name, quantity) {
-    request_gif(name, function (imageurl) {
-        var grid = document.querySelector('#columns');
-        var item = document.createElement('div');
-        var h = '<div class="col-xs-12 fridge-item">';
-        h += '<div class="fridge-item-pad">';
-        //h += '<img src="' + imageurl + '" class="giphy-image"></img>';
-        h += '<div class="item-text-thing-outer">';
-        h += '<div class="row item-text-thing">';
-        h += '<div class="col-xs-6 item-name">';
-        h += name;
-        h += '</div>';
-        h += '<div class="col-xs-6 item-count">';
-        h += quantity;
-        h += '</div>';
-        h += '</div>';
-        h += '</div>';
-        h += '</div>';
-        console.log(h);
-        salvattore['append_elements'](grid, [item]);
-        item.outerHTML = h;
-    })
 
+function makeChoice(title, callback) {
+    var htmlstr = '<div class="row itemrow">' +
+        '<div class="col-xs-8 big"><span class="vcenter">' + title + '</span></div>' +
+        '<div class="col-xs-4">' +
+        '<div class="input-group counter-spinner">' +
+        '<span class="input-group-btn">' +
+        '<button class="btn btn-lg btn-default down" type="button">-</button>' +
+        '</span>' +
+        '<input disabled type="text" class="form-control input-lg centered-text" placeholder="How much" value="0">' +
+        '<span class="input-group-btn">' +
+        '<button class="btn btn-lg btn-default up" type="button">+</button>' +
+        '</span>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="sep"></div>';
+    var element = $(htmlstr);
+    $('#choicehost').append(element);
+    callback(element);
 }
 
-console.log("Hello!");
-$.getJSON("/inventory/", function (data) {
-    console.log(data);
-    $(data.inventory).each(function (index, item) {
-        console.log(item);
-        append_fridge_content(item.name, item.quantity);
-    });
-
-});
-function request_gif(query, callback) {
-    $.getJSON("https://api.giphy.com/v1/gifs/search?q=" + query +"&api_key=dc6zaTOxFJmzC", function (data) {
-        // console.log(data.data[0]);
-        callback(data.data[0].images.downsized.url);
-    })
-
-}
+var choices = {};
+var newcount = 0;
 
 $(document).ready(function() {
-    // todo on-users
-    $('.otheruser').on('click', function(event) {
-        event.preventDefault();
-        console.log("wowza");
-        var newguy = $(this).html();
-        var oldguy = $('#activeuser').html();
-        $(this).html(oldguy);
-        $('#activeuser').html(newguy);
-    }); 
+    $.getJSON('/inventory/choices', function(data) {
+        $(data.suggestions).each(function(index, ttl) {
+            makeChoice(ttl, function(element) {
+                $(element).find('.up').click(function(e) {
+                    var cter = $(this).parent().siblings('input');
+                    var asint = parseInt(cter.val());
+                    choices[ttl] = asint + 1;
+                    cter.val(asint + 1);
+                });
+                $(element).find('.down').click(function(e) {
+                    var cter = $(this).parent().siblings('input');
+                    var asint = parseInt(cter.val());
+                    choices[ttl] = asint - 1;
+                    cter.val(asint - 1);
+                });
+            });
+        });
+    })
+    .fail(function() {
+        console.log("fu");
+    });
+    $.getJSON('/users', function(data) {
+        $(data.users).each(function(index, user) {
+            if(user.username == currentuser) {
+                $('#activeuser').html(currentuser);
+            } else {
+                var htmlstr = '<li><a href="#" class="otheruser">' + user.username + '</a></li>';
+                $('#userhost').append($(htmlstr));
+            }
+        });
 
-    // todo on-inventory
-    $('.counter-spinner').find('.up').click(function(e) {
+        // todo on-users
+        $('.otheruser').on('click', function(event) {
+            event.preventDefault();
+            console.log("wowza");
+            var newguy = $(this).html();
+            var oldguy = $('#activeuser').html();
+            $(this).html(oldguy);
+            $('#activeuser').html(newguy);
+        }); 
+    });
+
+    $('#submitbattn').click(function(e) {
+        var newthing = $('#newkram').val();
+        if(newthing !== '' && newcount > 0) {
+            choices[newthing] = newcount;
+        }
+
+        // transactions work upside down
+        $(Object.keys(choices)).each(function(index, k) {
+            choices[k] = -choices[k];
+        });
+
+        var data = {
+            username: $('#activeuser').html(),
+            transactions: choices
+        };
+        console.log(data);
+        $.ajax({
+            type: "POST",
+            url: '/events/',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            success: function () {
+                $('#submitbattn').text("YO GEIL!");
+                window.location.replace('/');
+            },
+            fail: function() {
+                alert("Schade schokolade");
+            }
+        });
+    });
+    
+    $('#philippcheckts').find('.up').click(function(e) {
         var cter = $(this).parent().siblings('input');
         var asint = parseInt(cter.val());
+        newcount++;
         cter.val(asint + 1);
     });
-    $('.counter-spinner').find('.down').click(function(e) {
+    $('#philippcheckts').find('.down').click(function(e) {
         var cter = $(this).parent().siblings('input');
         var asint = parseInt(cter.val());
+        newcount--;
         cter.val(asint - 1);
     });
 });
